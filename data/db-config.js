2ac3f9e6a0f329d165be8e3ec15b667cf19a1d02
escttsalo <escttsalo@gmail.com>
2021-07-09 00:39:30 -0600

    tweak

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 const configs = require('../knexfile.js');
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 
 module.exports = knex(configs[environment]);
