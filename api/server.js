b2fbc815155b5e310ac868941edd66f4136bc754
escttsalo <escttsalo@gmail.com>
2021-07-09 01:08:12 -0600

    final tweaks I think we got it

--- api/server.js
+++ api/server.js
@@ -1,18 +1,19 @@
 const path = require('path')
+const cors = require('cors')
 const express = require('express');
 const { logger, notFound, errorHandling } = require('./middleware/middleware')
 const usersRouter = require('./users/users-router') 
 const server = express();
-
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json())
-server.use(express.static(path.join(__dirname, 'client/build')))
+server.use(cors())
+server.use(express.static(path.join(__dirname, '../client/build')))
 // global middlewares and the user's router need to be connected here
 server.use(logger)
 server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
   // res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 // require your server and launch it
 require('dotenv').config()
-const cors = require('cors')
 const server = require('./api/server')
 
-server.use(cors())
-
 const PORT = process.env.PORT || 4000
-console.log(PORT)
 
 server.listen(PORT, () => {
     console.log(`\n Server Running on http://localhost:${PORT} \n`)
